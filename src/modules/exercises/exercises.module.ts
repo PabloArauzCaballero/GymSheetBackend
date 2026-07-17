a944353527ed6efe40f51e230f9e4a32152dc39c
Pablo Arauz Caballero <128090351+PabloArauzCaballero@users.noreply.github.com>
2026-07-17 17:12:00 -0400

    feat(exercises): register media services and equipment dependency

--- src/modules/exercises/exercises.module.ts
+++ src/modules/exercises/exercises.module.ts
@@ -1,17 +1,47 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { EquipmentModel } from '../equipment/equipment.model';
-import { AdminExercisesController, ExercisesController, UserExercisesController } from './exercises.controller';
+import { EquipmentModule } from '../equipment/equipment.module';
+import {
+  ExerciseMediaController,
+  ExerciseMediaManagementController,
+} from './exercise-media.controller';
+import { ExerciseMediaModel } from './exercise-media.model';
+import { ExerciseMediaRepository } from './exercise-media.repository';
+import { ExerciseMediaService } from './exercise-media.service';
 import { ExerciseEquipmentModel } from './exercise-equipment.model';
 import { ExerciseModel } from './exercise.model';
+import {
+  AdminExercisesController,
+  ExercisesController,
+  UserExercisesController,
+} from './exercises.controller';
 import { ExercisesRepository } from './exercises.repository';
 import { ExercisesService } from './exercises.service';
 import { UserExerciseModel } from './user-exercise.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([ExerciseModel, ExerciseEquipmentModel, UserExerciseModel, EquipmentModel])],
-  controllers: [ExercisesController, AdminExercisesController, UserExercisesController],
-  providers: [ExercisesRepository, ExercisesService],
-  exports: [ExercisesService, ExercisesRepository],
+  imports: [
+    EquipmentModule,
+    SequelizeModule.forFeature([
+      ExerciseModel,
+      ExerciseEquipmentModel,
+      UserExerciseModel,
+      ExerciseMediaModel,
+    ]),
+  ],
+  controllers: [
+    ExercisesController,
+    AdminExercisesController,
+    UserExercisesController,
+    ExerciseMediaController,
+    ExerciseMediaManagementController,
+  ],
+  providers: [
+    ExercisesRepository,
+    ExercisesService,
+    ExerciseMediaRepository,
+    ExerciseMediaService,
+  ],
+  exports: [ExercisesService, ExercisesRepository, ExerciseMediaRepository],
 })
 export class ExercisesModule {}
