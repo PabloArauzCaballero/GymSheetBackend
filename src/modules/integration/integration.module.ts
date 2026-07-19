1c12094bef36153c61fb68d13b9ef70db16176fc
Pablo Arauz Caballero <128090351+PabloArauzCaballero@users.noreply.github.com>
2026-07-19 14:14:31 -0400

    feat(events): expose domain event publisher from integration module

--- src/modules/integration/integration.module.ts
+++ src/modules/integration/integration.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { DomainEventModel } from './domain-event.model';
+import { DomainEventPublisher } from './domain-event.publisher';
+import { DomainEventRepository } from './domain-event.repository';
 import { OutboxJobModel } from './outbox-job.model';
 import { OutboxRepository } from './outbox.repository';
 import { OutboxService } from './outbox.service';
 
 @Module({
-  imports: [SequelizeModule.forFeature([OutboxJobModel])],
-  providers: [OutboxRepository, OutboxService],
-  exports: [OutboxRepository, OutboxService, SequelizeModule],
+  imports: [
+    SequelizeModule.forFeature([DomainEventModel, OutboxJobModel]),
+  ],
+  providers: [
+    DomainEventRepository,
+    DomainEventPublisher,
+    OutboxRepository,
+    OutboxService,
+  ],
+  exports: [
+    DomainEventRepository,
+    DomainEventPublisher,
+    OutboxRepository,
+    OutboxService,
+    SequelizeModule,
+  ],
 })
 export class IntegrationModule {}
