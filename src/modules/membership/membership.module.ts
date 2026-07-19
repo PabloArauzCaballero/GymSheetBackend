7164f2a31312fce6e5f3553302dee935c3fe3ef3
Pablo Arauz Caballero <128090351+PabloArauzCaballero@users.noreply.github.com>
2026-07-19 14:10:23 -0400

    fix(membership): wire credential-backed onboarding service

--- src/modules/membership/membership.module.ts
+++ src/modules/membership/membership.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { BusinessDateService } from '../../common/time/business-date.service';
+import { AccessCredentialModule } from '../access-control/access-credential.module';
 import { FacilitiesModule } from '../facilities/facilities.module';
 import { UsersModule } from '../users/users.module';
 import { CustomerProfileModel } from './customer-profile.model';
-import { AdminMembershipController, MembershipController } from './membership.controller';
+import { CustomerStaffService } from './customer-staff.service';
+import {
+  AdminMembershipController,
+  MembershipController,
+} from './membership.controller';
 import { MembershipPlanModel } from './membership-plan.model';
 import { MembershipModel } from './membership.model';
 import { MembershipRepository } from './membership.repository';
@@ -11,12 +17,32 @@ import { MembershipService } from './membership.service';
 import { PlanAccessScopeModel } from './plan-access-scope.model';
 import { StaffBranchScopeModel } from './staff-branch-scope.model';
 import { StaffProfileModel } from './staff-profile.model';
-import { BusinessDateService } from '../../common/time/business-date.service';
 
 @Module({
-  imports: [FacilitiesModule, UsersModule, SequelizeModule.forFeature([MembershipPlanModel, PlanAccessScopeModel, MembershipModel, CustomerProfileModel, StaffProfileModel, StaffBranchScopeModel])],
+  imports: [
+    AccessCredentialModule,
+    FacilitiesModule,
+    UsersModule,
+    SequelizeModule.forFeature([
+      MembershipPlanModel,
+      PlanAccessScopeModel,
+      MembershipModel,
+      CustomerProfileModel,
+      StaffProfileModel,
+      StaffBranchScopeModel,
+    ]),
+  ],
   controllers: [MembershipController, AdminMembershipController],
-  providers: [MembershipRepository, MembershipService, BusinessDateService],
-  exports: [MembershipRepository, MembershipService, BusinessDateService],
+  providers: [
+    MembershipRepository,
+    MembershipService,
+    CustomerStaffService,
+    BusinessDateService,
+  ],
+  exports: [
+    MembershipRepository,
+    MembershipService,
+    BusinessDateService,
+  ],
 })
 export class MembershipModule {}
